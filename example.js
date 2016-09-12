709bc0f88ec7c98e0af2608de1d280ef5d85ab63
sitypop <sityemilyshah@gmail.com>
2016-09-12 13:11:26 +0100

    Initial commit

---
+++ example.js
@@ -0,0 +1,5 @@
+var notifications = new Notifications({
+});
+notifications.push('HIYA POP!');
+notifications.push('What are you eating for dinner tonight?');
+notifications.push('Do you like mountains?');
